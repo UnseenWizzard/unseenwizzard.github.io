6ed910fb36ed5ca22fa1101767d0dc31a7761220
UnseenWizzard <nico@riedmann.dev>
2019-10-11 21:29:27 +0200

    Make timeline helper script more useable
    Do not create files starting with a dash as a result of wrongly calling
    the script
    
    Also add exec permission to file

--- newTimelineEntry.sh
+++ newTimelineEntry.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 FILENAME=./_posts/$1-01-01-$2.md
+if [[ -z $1 || -z $2 ]] 
+then
+	echo "please call as newTimelineEntry.sh {YEAR} {SHORT_TITLE}"
+	exit 1
+fi
+
 touch $FILENAME
 echo -e "--- \n layout: null \n title: $1 \n subtitle: $2\n image: \"img/timeline/default.jpg\" \n---\n" > $FILENAME
 vim $FILENAME
