942b54b1434d5386ae1264e661707c96fd5e889b
UnseenWizzard <nico@riedmann.dev>
2021-09-26 13:04:18 +0200

    remove sudo from local starter script

--- dockerServer.sh
+++ dockerServer.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --volume="$PWD:/srv/jekyll" -p "4000:4000" -it jekyll/minimal:latest jekyll serve --watch --force-polling
+docker run --rm --volume="$PWD:/srv/jekyll" -p "4000:4000" -it jekyll/minimal:latest jekyll serve --watch --force-polling
